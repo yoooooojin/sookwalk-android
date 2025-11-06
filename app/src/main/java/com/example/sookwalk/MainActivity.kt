03efd2497d6bd594798b51a743bb6d67c3b05ad3
yoooooojin <ujin4903@sookmyung.ac.kr>
2025-11-06 14:02:22 +0900

    ㅇㅇ

--- app/src/main/java/com/example/sookwalk/MainActivity.kt
+++ app/src/main/java/com/example/sookwalk/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.sookwalk.ui.theme.SookWalkTheme
 
 class MainActivity : ComponentActivity() {
