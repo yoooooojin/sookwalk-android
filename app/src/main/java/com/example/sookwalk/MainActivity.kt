2a8a67ba6d6fdc7de9f04f3fd0bf52efdeb0bf77
yoojinche <chesunny917@gmail.com>
2025-11-15 12:08:07 +0900

    ✨feat: dataStore 설정 및 화면 개발 구현

--- app/src/main/java/com/example/sookwalk/MainActivity.kt
+++ app/src/main/java/com/example/sookwalk/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.sookwalk.presentation.viewmodel.ThemeViewModel
 import com.example.sookwalk.ui.theme.SookWalkTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SookWalkTheme {
+            val themeVM: ThemeViewModel = hiltViewModel()
+            val isDark by themeVM.isDark.collectAsStateWithLifecycle()
+
+            SookWalkTheme (darkTheme = isDark){
 
             }
         }
