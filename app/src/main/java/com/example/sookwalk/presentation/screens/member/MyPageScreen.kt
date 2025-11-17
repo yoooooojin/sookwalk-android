8c817f12c0ef7b95880aa5ab718981cdaaeb8bd9
임지영 <141422840+naneunimjee@users.noreply.github.com>
2025-11-18 04:51:54 +0900

    🎨 style: TopBar import 경로 변경

--- app/src/main/java/com/example/sookwalk/presentation/screens/member/MyPageScreen.kt
+++ app/src/main/java/com/example/sookwalk/presentation/screens/member/MyPageScreen.kt
@@ -1,12 +1,9 @@
 package com.example.sookwalk.presentation.screens.member
 
-import android.R.attr.padding
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -23,7 +19,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -38,8 +33,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.sookwalk.screens.TopBar
 import com.example.sookwalk.R
+import com.example.sookwalk.presentation.components.TopBar
 
 
 @OptIn(ExperimentalMaterial3Api::class)
